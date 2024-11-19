873f2586a7599ec94fc3145b8ebd7dac474009f7
zhihaolaw <57338291+zhihaolaw@users.noreply.github.com>
2024-11-20 01:07:44 +0800

    Delete Patient_Management_system (1)/Patient_Management_system directory

--- Patient_Management_system (1)/Patient_Management_system/src/hospital_system/CSV_PathHandler.java
+++
@@ -1,7 +0,0 @@
-package hospital_system;
-
-public interface CSV_PathHandler {
-	
-	String toCSV();               // Converts the object to a CSV-formatted string
-    void fromCSV(String csvLine);  // Populates the object from a CSV line
-}
--- Patient_Management_system (1)/Patient_Management_system/src/hospital_system/LoginSystem.java
+++
@@ -1,8 +0,0 @@
-package hospital_system;
-
-public interface LoginSystem {
-    boolean login(String password);
-    void changePassword();
-    void showMenu();
-}
-
